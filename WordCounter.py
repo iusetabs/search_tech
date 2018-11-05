f344541c7cec9f07e8a25ec38ebb77b2db8e652c
iusetabs <jamesyboyoo@yahoo.ie>
2018-11-05 22:18:10 +0000

    two thirds of python script done. Need to add the stemming to the WordCounter.py. Also added a new test file.

--- WordCounter.py
+++ WordCounter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import operator
 import sys
 import re
 
@@ -8,21 +9,34 @@ def createWordsDict(fileName):
 	inputFile = open(fileName).read()
 	wordsDict = {}
 	delims= r'[ "\t,;.?!\r\n]+'
-	temp = re.split(delims,inputFile)
+	temp = filter(None, re.split(delims,inputFile))
 	for token in temp:
 		token = token.lower()
-		if token not in wordsDict:
+		if token not in wordsDict and token != None:
 			wordsDict[token] = 1
 		else:
 			wordsDict[token] += 1
 	return wordsDict
 
+def get_top_terms(wordsDict, k):
+	print sorted(wordsDict.items(), key=operator.itemgetter(1))[-int(k):][::-1]
+	return sorted(wordsDict.items(), key=operator.itemgetter(1))[-int(k):][::-1]
+	
+
 def main():
 	try:
 		fileName = sys.argv[1]
-		wordsDict = createWordsDict(fileName)
-		for key, item in wordsDict.iteritems():
-			print "Key: ", key, item
+		k = 5
+		if len(sys.argv) > 2:
+			k = int(sys.argv[2])
+		wordsDict = get_top_terms(createWordsDict(fileName), k)
+		print "The top ", k, " terms are:\n" 
+		i = 0
+		for item in wordsDict:
+			print "Rank #", k-k+i+1, " : ", item[0], "\n\tOccurances: ", item[1], "\n"
+			i+=1
+
+		print "\n---Done.\n"
 	except Exception, err:
 		print Exception, err
 
